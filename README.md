49abad1da1aef4da71b8e80ffb55e1787308ff3a
mterpstra <mark.terpstra.mt@gmail.com>
2016-12-22 17:02:25 -0500

    Initial working commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # simple-http-https-proxy
 Very simple proxy to allow http to https traffic with logging.
+
+## Work in Progress ##
+When looking for a simple way to pass HTTP traffic to a proxy and have
+that proxy forward it to an HTTPS service, I did not find any very
+simple solutions out there.  It seems as nginx could do something like 
+it, but again, it was too much effort.  This is a very simple, with 
+no error checking, but working version of http to https proxy.
+
+## Build Instructions ##
+go build
+
+## Usage ##
+./simple-http-https-proxy {Your Host Name Here}
+
+## What it does ##
+1. Listens on localhost:8080
+2. Accepts requests
+3. Dumps the rquest to the console
+4. Replace Host header with Argv[1]
+5. Replace http with https
+6. Sends the request to the server at Argv[1]
+7. Dumps server response
+8. Sends response back to caller
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+)
+
+var host string
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	// Dump the original request to the terminal
+	println("-- In From Client --------------------------------------------------------------------------")
+	b, _ := httputil.DumpRequest(r, true)
+	println(string(b))
+	println("--------------------------------------------------------------------------------------------\n\n")
+
+	// Make the new request for the server using HTTPS
+	req := &http.Request{
+		Method:     r.Method,
+		URL:        r.URL,
+		Header:     r.Header,
+		Proto:      r.Proto,
+		ProtoMajor: r.ProtoMajor,
+		ProtoMinor: r.ProtoMinor,
+		Body:       r.Body,
+	}
+
+	// Override only two values
+	req.URL.Scheme = "https"
+	req.URL.Host = host
+
+	// Dump the new request to the server out to the terminal
+	println("-- Out to Server ---------------------------------------------------------------------------")
+	b, _ = httputil.DumpRequest(req, true)
+	println(string(b))
+	println("--------------------------------------------------------------------------------------------\n\n")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		// @todo:  Something should be sent down to the client
+		println(err.Error())
+		return
+	}
+
+	// Dump the server response
+	println("-- Response from Server --------------------------------------------------------------------")
+	b, _ = httputil.DumpResponse(resp, true)
+	println(string(b))
+	body, err := ioutil.ReadAll(resp.Body)
+	println("--------------------------------------------------------------------------------------------\n\n")
+
+	// Write the response back to the client
+	w.Write(body)
+}
+
+// @todo: Write some help commands
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	host = args[0]
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
